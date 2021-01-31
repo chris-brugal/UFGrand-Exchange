c17505bb9183660fc66b900537b064fec9d4dc5c
chris-brugal <chrisbrugal@yahoo.com>
2021-01-31 01:14:17 -0500

    added login and sign up functionality and cryptology

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 
 import postRoutes from './routes/posts.js';
+import userRouter from './routes/users.js'
 
 
 
@@ -14,6 +15,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use("/user", userRouter);
 
 const CONNECTION_URL = 'mongodb+srv://universal:universaL1!@cluster0.hg7zp.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
