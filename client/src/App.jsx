a12ff304a232f87edb2ca0bcdd4e7375376254a9
Josh Fu <fu.joshua2001@gmail.com>
2021-01-31 05:20:50 -0500

    fixed imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { Grid, Container, IconButton, Typography, AppBar, Toolbar, InputBase, MenuItem, Menu}  from '@material-ui/core';
+import { Grid, Container}  from '@material-ui/core';
 
 import Navbar from './components/NavBar/NavBar';
 import useStyles from './styles'
 import Posts from './components/Posts/Posts';
+import { getPosts } from './actions/posts';
 import Form from './components/Form/Form';
+import { useDispatch } from 'react-redux';
 
 export default function PrimarySearchAppBar() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const [currentId, setCurrentId] = useState(0);
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+
   return (
     <div className={classes.grow}>
       <Navbar />
