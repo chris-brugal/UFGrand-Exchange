ae0322caa82595f04ca3b09742d9bf03079670ac
chris-brugal <chrisbrugal@yahoo.com>
2021-01-30 22:05:06 -0500

    updated everything

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,267 @@
+// import React from 'react';
+// import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+// import Card from './components/SimpleCard';
+// import useStyles from './styles';
+// import Button from '@material-ui/core/Button';
+// import SearchBar from'./components/SearchBar.jsx'
+// import DehazeIcon from '@material-ui/icons/Dehaze';
+// import IconButton from '@material-ui/core/IconButton';
+
+// const App = () => {
+//     const classes = useStyles();
+//     return (
+//         <Container maxidth="lg">
+//             <AppBar className={classes.appBar} position="sticky" color="inherit">
+//                 <DehazeIcon edge="start" />
+//                 <Typography className={classes.heading} varient="h2" align="center">Class Exchange</Typography>
+//                 <SearchBar/>
+//                 <Button className={classes.Button} variant="contained" color="primary" size="small">Search</Button>
+//                 <Button className={classes.Button} variant="contained" color="primary" size="small">
+//                     Login
+//                 </Button>
+//             </AppBar>
+
+//             <Grow in>
+//                 <Container>
+//                     <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+//                         <Grid item xs={4}>
+//                             <Card/>
+//                             <Card/>
+//                             <Card/>
+//                         </Grid>
+//                         <Grid item xs={4}>
+//                             <Card/>
+//                         </Grid>
+//                         <Grid item xs={4}>
+//                             <Card/>
+//                         </Grid>
+//                     </Grid>
+//                 </Container>
+//             </Grow>
+//         </Container>
+//     );
+// }
+
 import React from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import { Grid, Container, IconButton, Typography, AppBar, Toolbar, InputBase, MenuItem, Menu}  from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MoreIcon from '@material-ui/icons/MoreVert';
 
 import Card from './components/SimpleCard';
-import memories from './images/memories.png';
-import useStyles from './styles';
-import Button from '@material-ui/core/Button';
-const App = () => {
-    const classes = useStyles();
-    return (
-        <Container maxidth="lg">
-            <AppBar className={classes.appBar} position="sticky" color="inherit">
-                <Typography className={classes.heading} varient="h2" align="center">Class Exchange</Typography>
-                <img className={classes.image} src={memories} alt="memories" height="60" />
-                <Button className={classes.Button} variant="contained" color="primary">Search</Button>
-                <Button className={classes.Button} variant="contained" color="primary">
-                    Login
-                </Button>
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={4}>
-                            <Card/>
-                            <Card/>
-                            <Card/>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Card/>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Card/>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: '70%',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+}));
+
+export default function PrimarySearchAppBar() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>My Account</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Log Out</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            Grand Exchange
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Find Classes"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              onKeyDown={(e) => {
+                  if(e.key === "Enter") {
+                      console.log("Hello World!");
+                  }
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+        <Container>
+            <Grid container alignItems="stretch" spacing={5}>
+                <Grid container item xs={4}>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                </Grid>
+                <Grid item xs={4}>
+                    <Card/>
+                </Grid>
+
+                <Grid item xs={4}>
+                    <Card/>
+                </Grid>
+            </Grid>
         </Container>
-    );
+    </div>
+  );
 }
 
-export default App
+//export default App
