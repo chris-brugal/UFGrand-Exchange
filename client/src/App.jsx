33b1ec122634d8c92f45348d40c99a2cc9220ee0
jenny <jenny.xie@ufl.edu>
2021-01-30 23:37:04 -0500

    broken

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,77 +1,12 @@
 import React from 'react';
-import { fade, makeStyles } from '@material-ui/core/styles';
 import { Grid, Container, IconButton, Typography, AppBar, Toolbar, InputBase, MenuItem, Menu}  from '@material-ui/core';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import MoreIcon from '@material-ui/icons/MoreVert';
 
-import Card from './components/SimpleCard';
+import Card from './components/Posts/Posts';
 import useStyles from './styles'
-
-// const useStyles = makeStyles((theme) => ({
-  // grow: {
-  //   flexGrow: 1,
-  // },
-  // menuButton: {
-  //   marginRight: theme.spacing(2),
-  // },
-  // title: {
-  //   display: 'none',
-  //   [theme.breakpoints.up('sm')]: {
-  //     display: 'block',
-  //   },
-  // },
-  // search: {
-  //   position: 'relative',
-  //   borderRadius: theme.shape.borderRadius,
-  //   backgroundColor: fade(theme.palette.common.white, 0.15),
-  //   '&:hover': {
-  //     backgroundColor: fade(theme.palette.common.white, 0.25),
-  //   },
-  //   marginRight: theme.spacing(2),
-  //   marginLeft: 0,
-  //   width: '100%',
-  //   [theme.breakpoints.up('sm')]: {
-  //     marginLeft: theme.spacing(3),
-  //     width: '70%',
-  //   },
-  // },
-  // searchIcon: {
-  //   padding: theme.spacing(0, 2),
-  //   height: '100%',
-  //   position: 'absolute',
-  //   pointerEvents: 'none',
-  //   display: 'flex',
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  // },
-  // inputRoot: {
-  //   color: 'inherit',
-  // },
-  // inputInput: {
-  //   padding: theme.spacing(1, 1, 1, 0),
-  //   // vertical padding + font size from searchIcon
-  //   paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-  //   transition: theme.transitions.create('width'),
-  //   width: '100%',
-  //   [theme.breakpoints.up('md')]: {
-  //     width: '20ch',
-  //   },
-  // },
-  // sectionDesktop: {
-  //   display: 'none',
-  //   [theme.breakpoints.up('md')]: {
-  //     display: 'flex',
-  //   },
-  // },
-  // sectionMobile: {
-  //   display: 'flex',
-  //   [theme.breakpoints.up('md')]: {
-  //     display: 'none',
-  //   },
-  // },
-// }));
 
 export default function PrimarySearchAppBar() {
   const classes = useStyles();
---
+++ client/src/components/Posts/Post/Post.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Card, CardActions, CardContent, Button, Typography} from '@material-ui/core';
+
+
+import useStyles from './style'
+
+const Post = () => {
+    const classes = useStyles();
+
+    return (
+
+    );
+}
+
+export default Card
